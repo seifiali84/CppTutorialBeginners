adc1954da2145066475d0491e62feb5fdac8676b
Ali Seifi <seifiali84@gmail.com>
2024-03-23 23:09:18 +0330

    Add 0 Before First Steps Folder Names

---
+++ 10 - Functions/Program.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void WriteSeparator()
+{
+    cout << "----------"
+         << "\n";
+    cout << "**********"
+         << "\n";
+    cout << "----------"
+         << "\n";
+}
+
+string Hi()
+{
+    return "salam";
+}
+
+float PI()
+{
+    return 3.14;
+}
+
+void SayHi(string Name)
+{
+    cout << "Hi " << Name << "!\n";
+}
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+void Hello(string Name = "World")
+{
+    cout << "Hello " << Name << "\n";
+}
+
+void ShowMembers(string Names[])
+{
+
+    for (int i = 0; i < Names->size(); i++)
+    {
+        cout << i + 1 <<  " - " << Names[i] << "\n";
+    }
+}
+
+int main()
+{
+    cout << "Hi Ali"
+         << "\n";
+    WriteSeparator();
+    cout << "Hi Amin"
+         << "\n";
+    WriteSeparator();
+    cout << "Hi Arshia"
+         << "\n";
+
+    cout << Hi() << "\n";
+
+    int r = 2;
+    float Area = PI() * r * r;
+    cout << Area << "\n";
+
+    SayHi("Ali");
+    SayHi("Amin");
+
+    cout << Sum(1, 2) << "\n";
+
+    Hello("Ali");
+    Hello();
+
+    string Names[] = {"Ali", "Arshia", "Amin"};
+    ShowMembers(Names);
+
+    return 0;
+}
